1b8b4d30f4ffe9de163cefcc42536c7390a453f8
koloqial <koloqial@icloud.com>
2025-04-05 09:52:30 +0100

    Comments, and adding test for Controller.

--- AFIRegistrationAPITests/Controllers/CustomerControllerTests.cs
+++ AFIRegistrationAPITests/Controllers/CustomerControllerTests.cs
@@ -1,7 +1,11 @@
-﻿using AFIRegistration.Requests;
+﻿using System.Net;
+using AFIRegistration.Requests;
 using AFIRegistration.Validation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using Xunit.Sdk;
 
 namespace AFIRegistrationAPITests;
 
@@ -10,8 +14,11 @@ public class CustomerControllerTests
     private CustomerController _controller;
     private void SetUp()
     {
+        var options = new DbContextOptionsBuilder<RegistrationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestRegistrationDb")
+            .Options;
         var validator = new RegistrationRequestValidator();
-        var mockDb = Mock.Of<RegistrationDbContext>();
+        var mockDb = new RegistrationDbContext(options);
         _controller = new CustomerController(mockDb, validator);
     }
 
@@ -19,8 +26,21 @@ public class CustomerControllerTests
     public async Task CustomerController_SendCorrectData_RecieveRecordId()
     {
 
+        // Arrange
         SetUp();
-        Assert.True(true);
+        var user = new RegistrationRequest()
+        {
+            FirstName = "Garfield",
+            LastName = "Look",
+            PolicyNumber = "XX-090909",
+            DateOfBirth = "2004-12-01"
+        };
 
+        // Act
+        var result = await _controller.Post(user);
+
+        // Assert
+        Assert.True(true);
+        Assert.IsType<Created>(result);
     }
 }
--- AFIRegistrationAPITests/Validators/RegistrationRequestValidatorTests.cs
+++ AFIRegistrationAPITests/Validators/RegistrationRequestValidatorTests.cs
@@ -16,6 +16,7 @@ public class RegistrationRequestValidatorTests
     [Fact]
     public void Should_fail_DOB_validation()
     {
+        // Arrange
         Setup();
         var model = new RegistrationRequest
         {
@@ -24,8 +25,9 @@ public class RegistrationRequestValidatorTests
             PolicyNumber = "XX-999999",
             DateOfBirth = "2020-09-09"
         };
+        // Act
         var result = validator.TestValidate(model);
-
+        // Assert
         Assert.False(result.IsValid);
         Assert.NotEmpty(result.Errors);
         Assert.Contains(result.Errors, x => x.ErrorMessage == ErrorMessages.MUST_BE_OVER_18);
@@ -34,6 +36,7 @@ public class RegistrationRequestValidatorTests
     [Fact]
     public void Should_pass_DOB_validation()
     {
+        // Arrange
         Setup();
         var model = new RegistrationRequest
         {
@@ -42,8 +45,9 @@ public class RegistrationRequestValidatorTests
             PolicyNumber = "XX-999999",
             DateOfBirth = "2005-09-09"
         };
+        // Act
         var result = validator.TestValidate(model);
-
+        // ASsert
         Assert.True(result.IsValid);
         Assert.Empty(result.Errors);
     }
@@ -51,8 +55,8 @@ public class RegistrationRequestValidatorTests
     [Fact]
     public void Should_fail_email__tld()
     {
+        // Arrange
         Setup();
-
         var model = new RegistrationRequest
         {
             FirstName = "Gary",
@@ -60,8 +64,9 @@ public class RegistrationRequestValidatorTests
             PolicyNumber = "XX-999999",
             Email = "ggga@me.co.il"
         };
+        // Act
         var result = validator.TestValidate(model);
-
+        // Assert
         Assert.False(result.IsValid);
         Assert.NotEmpty(result.Errors);
         Assert.Contains(result.Errors, x => x.ErrorMessage == ErrorMessages.INVALID_TLD);
@@ -70,8 +75,8 @@ public class RegistrationRequestValidatorTests
     [Fact]
     public void Should_fail_email_local()
     {
+        // Arrange
         Setup();
-
         var model = new RegistrationRequest
         {
             FirstName = "Gary",
@@ -79,8 +84,9 @@ public class RegistrationRequestValidatorTests
             PolicyNumber = "XX-999999",
             Email = "ggg@me.co.uk"
         };
+        // Act
         var result = validator.TestValidate(model);
-
+        // Assert
         Assert.False(result.IsValid);
         Assert.NotEmpty(result.Errors);
         Assert.Contains(result.Errors, x => x.ErrorMessage == ErrorMessages.INVALID_LOCAL_PART);
@@ -89,8 +95,8 @@ public class RegistrationRequestValidatorTests
     [Fact]
     public void Should_fail_email_local_and_tld()
     {
+        // Arrange
         Setup();
-
         var model = new RegistrationRequest
         {
             FirstName = "Gary",
@@ -98,8 +104,9 @@ public class RegistrationRequestValidatorTests
             PolicyNumber = "XX-999999",
             Email = "ggg@me.co.il"
         };
+        // Act
         var result = validator.TestValidate(model);
-
+        // Assert
         Assert.False(result.IsValid);
         Assert.NotEmpty(result.Errors);
         Assert.Contains(result.Errors, x => x.ErrorMessage == ErrorMessages.INVALID_LOCAL_PART);
