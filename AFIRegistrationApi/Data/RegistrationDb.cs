2072d15311bb573594e279aa62a0b4b160b3dc93
koloqial <koloqial@icloud.com>
2025-04-05 03:49:39 +0100

    Database Context

--- AFIRegistrationApi/Data/RegistrationDb.cs
+++ AFIRegistrationApi/Data/RegistrationDb.cs
@@ -3,8 +3,11 @@ using AFIRegistration.Models;
 public class RegistrationDbContext : DbContext
 {
     public DbSet<Customer> Customer { get; set; }
+    public DbSet<CustomerEmail> CustomerEmail { get; set; }
+    public DbSet<CustomerDOB> CustomerDOB { get; set; }
 
     public RegistrationDbContext(DbContextOptions<RegistrationDbContext> options)
             : base(options)
     { }
+
 }
