e757cd9112e495d2797a21393d98d4d52450dbeb
koloqial <koloqial@icloud.com>
2025-04-05 03:49:16 +0100

    Adding business logic validation to DB model. Spelling mistakes.

--- AFIRegistrationApi/Models/Customer.cs
+++ AFIRegistrationApi/Models/Customer.cs
@@ -3,8 +3,9 @@ namespace AFIRegistration.Models;
 public class Customer
 {
     public int Id { get; set; }
-    public required string FrstName { get; set; }
+    public required string FirstName { get; set; }
     public required string LastName { get; set; }
+    public required string PolicyNumber { get; set; }
 
     public CustomerDOB? DOB { get; set; }
     public CustomerEmail? Email { get; set; }
--- AFIRegistrationApi/Models/CustomerDOB.cs
+++ AFIRegistrationApi/Models/CustomerDOB.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AFIRegistration.Models;
 
-public class CustomerDOB
+public class CustomerDOB : IValidatableObject
 {
+    public int Id { get; set; }
     public int CustomerId { get; set; }
 
-    //Unix Timestamp
-    public int DOB { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (DateOfBirth != null && DateTime.UtcNow.AddYears(-18) < DateOfBirth)
+        {
+            yield return new ValidationResult("You must be over 18 in order to register.", [nameof(DateOfBirth)]);
+        }
+
+    }
 }
